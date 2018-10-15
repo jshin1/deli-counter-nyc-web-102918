94bc23456ffa74ea8209f310f21ff37b992d0eea
Ji S <j2yshin@gmail.com>
2018-10-15 23:32:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    "The line is currently empty."
+  else
+    counter = 0
+    phrase = "The line is currently: "
+    while counter < katz_deli.size
+      phrase += "#{counter + 1}. #{katz_deli[counter]}"
+      counter += 1
+    end
+    phrase
+  end
+end
